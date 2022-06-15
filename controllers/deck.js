e458860a57bd1fe9a299c3ced3af23f45b0cbc0b
mrtnmmn <mrtn.lmba@gmail.com>
2022-06-15 20:28:19 +0200

    added deck edition

--- controllers/deck.js
+++ controllers/deck.js
@@ -47,14 +47,16 @@ async function addOne(req,res) {
     const editing = req.body.editing
 
     console.log(newDeck)
+    console.log(req.body.deck)
 
     try {
 
         if (editing) {
+            console.log(req.body.deck._id)
             console.log('update')
-            let updatedDeck = await Deck.replaceOne({_id: newDeck._id}, newDeck)
-            console.log(updatedDeck)
-            res.status(200).json({action: 'add one deck', data: updatedDeck})
+            let deletedDeck = await Deck.findByIdAndDelete(req.body.deck._id)
+            let savedDeck = await newDeck.save()
+            res.status(200).json({action: 'add one deck', data: savedDeck})
         } else {
             console.log('new')
             let savedDeck = await newDeck.save()
